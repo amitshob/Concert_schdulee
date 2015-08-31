f8e352fd203bb10752aaa4172832fc6c2bd08962
amitshob <amitshob@gmail.com>
2015-08-31 08:05:18 -0700

    it will add tests -callback, presence validation

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,1 +1,16 @@
 require('spec_helper')
+
+describe(Band) do
+  
+  it("tests capitalization of first letter (callback functionality ") do
+    test_band = Band.create({:name => "tool", :rating=> "3"})
+    expect(test_band.name()).to(eq("Dethklok"))
+  end
+  
+  it("tests presence validation") do
+    test_band = Band.new({:name => ""})
+    expect(test_band.save()).to(eq(false))
+  end
+
+  
+end
