cc1ad9502d8496e518a87649a15bfeb002e18eb3
amitshob <amitshob@gmail.com>
2015-08-31 09:44:18 -0700

    added specs for callback and validation

---
+++ spec/venue_spec.rb
@@ -0,0 +1,13 @@
+require('spec-helper')
+describe(venue) do
+  
+  it("tests capitalization of first letter (callback functionality ") do
+    test_band = Venue.create({:name => "coliseum"})
+    expect(test_venue.name()).to(eq("Coliseum"))
+  end
+  
+  it("tests presence validation") do
+    test_venue = Venue.new({:name => ""})
+    expect(test_venue.save()).to(eq(false))
+  end
+end 
