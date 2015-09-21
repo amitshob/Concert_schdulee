ec735c96a6f31d268bcfa1f28ca7fc11a7b1e49e
amitshob <amitshob@gmail.com>
2015-09-21 08:25:04 -0700

    Update band.rb

--- lib/band.rb
+++ lib/band.rb
@@ -1,10 +1,9 @@
 class Band < ActiveRecord::Base
 
   has_and_belongs_to_many(:venues)
-<<<<<<< HEAD
-=======
+
   validates(:name, {:presence => true})
->>>>>>> c3a7b01c02574eb9badd1707a3e611f610b5dd17
+
   before_save(:capitalize_name)
   private
 
@@ -13,8 +12,3 @@ class Band < ActiveRecord::Base
   end
   #has_many: :ingredients, through: :ingredients_bands
 end
-<<<<<<< HEAD
-=======
-
- 
->>>>>>> c3a7b01c02574eb9badd1707a3e611f610b5dd17
