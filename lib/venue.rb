6fd13df0ef47fc8f6d817cc9ae6d9cd8fec2dca8
amitshob <amitshob@gmail.com>
2015-09-21 07:39:00 -0700

    Update venue.rb

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,12 +1,8 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many(:bands)
-<<<<<<< HEAD
-=======
   validates(:name, {:presence => true})
->>>>>>> c3a7b01c02574eb9badd1707a3e611f610b5dd17
   before_save(:capitalize_name)
   private
-
   define_method(:capitalize_name) do
     self.name=(name().capitalize())
   end
